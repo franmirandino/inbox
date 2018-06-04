3ac83b1fd9ad6858addc4b98395e7d9937783615
Francisco Miranda <franmirandino@gmail.com>
2018-06-04 17:06:42 -0400

    vendedores

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>{{ config('config.app') }}</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="/dashboard/vendor/bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome CSS-->
+    <link rel="stylesheet" href="/dashboard/vendor/font-awesome/css/font-awesome.min.css">
+    <!-- Fontastic Custom icon font-->
+    <link rel="stylesheet" href="/dashboard/css/fontastic.css">
+    <!-- Google fonts - Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+    <!-- jQuery Circle-->
+    <link rel="stylesheet" href="/dashboard/css/grasp_mobile_progress_circle-1.0.0.min.css">
+    <!-- Custom Scrollbar-->
+    <link rel="stylesheet" href="/dashboard/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="/dashboard/css/style.default.css" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="/dashboard/css/style.red.css">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="/dashboard/img/favicon.ico">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+  </head>
+  <body>
+    <!-- Side Navbar -->
+    <nav class="side-navbar">
+      <div class="side-navbar-wrapper">
+        <!-- Sidebar Header    -->
+        <div class="sidenav-header d-flex align-items-center justify-content-center">
+          <!-- User Info-->
+          <div class="sidenav-header-inner text-center"><img src="/dashboard/img/images.png" alt="DEUCE" class="img-fluid rounded" >
+            <h2 class="h5">Francisco Miranda</h2><span>Web Developer</span>
+          </div>
+          <!-- Small Brand information, appears on minimized sidebar-->
+          <div class="sidenav-header-logo"><a href="index.html" class="brand-small text-center"> <strong>B</strong><strong class="text-primary">D</strong></a></div>
+        </div>
+        <!-- Sidebar Navigation Menus-->
+        <div class="main-menu">
+          <h5 class="sidenav-heading">Principal</h5>
+          <ul id="side-main-menu" class="side-menu list-unstyled">                                         
+            <li class="active"><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="fas fa-address-card"></i>Funcionarios </a>
+              <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
+                <li><a href="{{ route('vendedores.index') }}">Vendedores</a></li>
+                <li><a href="#">Técnicos</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>  
+      </div>
+    </nav>
+    <div class="page">
+      <!-- navbar-->
+      <header class="header">
+        <nav class="navbar">
+          <div class="container-fluid">
+            <div class="navbar-holder d-flex align-items-center justify-content-between">
+              <div class="navbar-header"><a id="toggle-btn" href="#" class="menu-btn"><i class="icon-bars"> </i></a><a href="index.html" class="navbar-brand">
+                  <div class="brand-text d-none d-md-inline-block"><span>DEUCE S.A. </span><strong class="text-primary"> Servicios</strong></div></a></div>
+            </div>
+          </div>
+        </nav>
+      </header>
+
+      @yield('content')
+
+
+      <footer class="main-footer">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm-6">
+              <p>DEUCE SA &copy; 2016-2019</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+    <!-- JavaScript files-->
+    <script src="/dashboard/vendor/jquery/jquery.min.js"></script>
+    <script src="/dashboard/vendor/popper.js/umd/popper.min.js"> </script>
+    <script src="/dashboard/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/dashboard/js/grasp_mobile_progress_circle-1.0.0.min.js"></script>
+    <script src="/dashboard/vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="/dashboard/vendor/chart.js/Chart.min.js"></script>
+    <script src="/dashboard/vendor/jquery-validation/jquery.validate.min.js"></script>
+    <script src="/dashboard/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js"></script>
+    <!-- Main File-->
+    <script src="/dashboard/js/front.js"></script>
+  </body>
+</html>
---
+++ resources/views/vendedores/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+
+@section('content')
+	
+	<h1>Vendedores</h1>
+
+@endsection
